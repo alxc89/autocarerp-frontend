4b7d8440c334741e9deb70ad63c3a697cc01c6f7
alxc89 <alexcaardoso@gmail.com>
2025-12-11 22:15:41 -0300

    ajustando withCredentials para false

--- src/lib/api-client.ts
+++ src/lib/api-client.ts
@@ -8,7 +8,7 @@ const apiClient: AxiosInstance = axios.create({
     headers: {
         'Content-Type': 'application/json',
     },
-    withCredentials: true,
+    withCredentials: false,
 });
 
 // Token management
